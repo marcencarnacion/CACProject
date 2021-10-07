edd9b439c2478374768198477b46235da4e0453d
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2021-10-07 08:48:47 -0700

    vertical layout for home page + short messages

--- app/src/main/java/com/example/groupcac2021/MainActivity.java
+++ app/src/main/java/com/example/groupcac2021/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.groupcac2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,21 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    } // end main
+
+    public void groceryClick (View v)
+    {
+        Toast.makeText(this, "You clicked the budget button!", Toast.LENGTH_SHORT).show();
     }
-}
+
+    public void govClick (View v)
+    {
+        Toast.makeText(this, "You clicked the gov program button!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void couponClick (View v)
+    {
+        Toast.makeText(this, "You clicked the coupon button!", Toast.LENGTH_SHORT).show();
+    }
+
+} // end main class
