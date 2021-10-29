e25b3f2b2fa27f0d5751b05a5ac77d1bd06a827a
ThanosKirby18 <emailiano.p@gmail.com>
2021-10-29 08:33:11 -0700

    we have websites that have helpful coupouns

--- app/src/main/java/com/example/groupcac2021/MainActivity.java
+++ app/src/main/java/com/example/groupcac2021/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.groupcac2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void browser1(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.walmart.com/help/article/print-and-use-coupons/205b65e070f247d7a98687b7ff2d5cc9"));
+        startActivity(browserIntent);
+    }
+
+    public void browser2(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://coupons.staterbros.com/"));
+        startActivity(browserIntent);
+
+    }
+    public void browser3(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.target.com/c/top-deals/-/N-4xw74"));
+        startActivity(browserIntent);
+    }
+    //
+    public void browser4(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://superiorgrocers.com/specials/"));
+        startActivity(browserIntent);
+    }
+    //
 }
