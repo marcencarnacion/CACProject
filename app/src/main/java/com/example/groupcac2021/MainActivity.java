431740fc00a814539999bf0397e73be2be0c06f1
91091791 <91091791@mvusd.net>
2021-10-29 08:51:41 -0700

    Marc's part

--- app/src/main/java/com/example/groupcac2021/MainActivity.java
+++ app/src/main/java/com/example/groupcac2021/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.groupcac2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void browser1(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.usa.gov/"));
+        startActivity(browserIntent);
+    }
+    public void browser2(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.benefits.gov/"));
+        startActivity(browserIntent);
+    }
+    public void browser3(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.grants.gov/"));
+        startActivity(browserIntent);
     }
 }
